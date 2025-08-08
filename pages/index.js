8b1ea5a1cbbc5b991e0155da58750dc58688ed81
Swastik-ML <68067649+Swastik-ML@users.noreply.github.com>
2025-08-08 08:51:13 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 import { useEffect, useState } from 'react';
 import { useRouter } from 'next/router';
-import { supabase } from '../utils/supabaseClient';
+import { supabase } from '../lib/supabaseClient';
 
 export default function Home() {
   const router = useRouter();
